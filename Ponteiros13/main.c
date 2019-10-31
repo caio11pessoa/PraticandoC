9bd90d431ab925b2f01e8d69f9e002d0f30d65e1
Caio Pessoa <caiopessoa1298@gmail.com>
2019-10-31 05:17:04 -0300

    Criada a pasta Ponteiros13 onde está o conteúdo da aula 13

---
+++ Ponteiros13/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void copiarString(char *copiarDaqui, char *colarAqui){
+
+    while(*copiarDaqui != '\0'){
+        *colarAqui = *copiarDaqui;
+        colarAqui ++;
+        copiarDaqui ++;
+    }
+    *colarAqui = '\0';
+}
+int main()
+{
+    void copiarString(char *copiarDaqui, char *colarAqui);
+
+    char string1[] = "Pao com mortadela";
+    char string2[20];
+
+    copiarString(string1, string2);
+    printf("%s\n", string2);
+
+    getchar();
+    return 0;
+}
